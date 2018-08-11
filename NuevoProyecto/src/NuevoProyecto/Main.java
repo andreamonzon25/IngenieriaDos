c7b295e188d80b6d37a88f5e92332f32841b1309
Andrea <Andrea@N13W101S>
2018-08-10 22:11:18 -0300

    VERSION 2

--- NuevoProyecto/src/NuevoProyecto/Main.java
+++ NuevoProyecto/src/NuevoProyecto/Main.java
@@ -1,69 +1,137 @@
 package NuevoProyecto;
+
 import java.util.Scanner;
+
 public class Main {
+
 	public static void main(String[] args) {
+
 	    {
+
 	        Scanner LEER = new Scanner(System.in);
+
 	        int A,B,C,D;
 
-	        System.out.print("INGRESE EL NUMERO 01 : ");
+
+
+	        System.out.print("INGRESE EL PRIMER NUMERO: ");
+
 	        A = LEER.nextInt();
-	        System.out.print("INGRESE EL NUMERO 02 : ");
+
+	        System.out.print("INGRESE EL SEGUNDO NUMERO: ");
+
 	        B = LEER.nextInt();
-	        System.out.print("INGRESE EL NUMERO 03 : ");
+
+	        System.out.print("INGRESE EL TERCER NUMERO: ");
+
 	        C = LEER.nextInt();
-	        System.out.print("INGRESE EL NUMERO 04 : ");
+
+	        System.out.print("INGRESE EL CUARTO NUMERO: ");
+
 	        D= LEER.nextInt();
 
+
+
 	        System.out.println("\nLOS NUMEROS DE MAYOR A MENOR SON: ");
+
 	        
+
 	        if(A >= B && A >= C && A >= D)
+
 	        {
+
 	            if(B >= C && B >= D)
+
 	            	if (C>= D)
+
 	            {
+
 	                System.out.println("\n" + A + "\n" + B + "\n" + C+ "\n" + D);
+
 	            }
+
 	            else
+
 	            {
+
 	                System.out.println("\n" + A + "\n" + B + "\n" + D+ "\n" + C);
+
 	            }
+
 	        }
+
 	        else if(B >= A && B >= C  && B >= D){
+
 	        	 if(A >= C && A >= D)
+
 		            	if (C>= D)
+
 	            {
+
 	                System.out.println("\n" + B + "\n" + A + "\n" + C+ "\n" + D);
+
 	            }
+
 	            else
+
 	            {
+
 	                System.out.println("\n" + B + "\n" + A +"\n" + D+ "\n" + C);
+
 	            }
+
 	        }
+
 	        else if(C >= A && C >= B  && C >= D)
+
 	        {
+
 	        	 if(A >= B && A>= D)
+
 		            	if (B>= D) {
+
 	                System.out.println("\n" + C + "\n" + A + "\n" + B+"\n" + D);
+
 	            }
+
 	            else
+
 	            {
+
 	                System.out.println("\n" + C + "\n" + A + "\n" + D+ "\n" + B);
+
 	            }
+
 	        }   
+
 	        else if(D >= A && D >= B  && D >= C)
+
 	        {
+
 	        	 if(A >= B && A>= C)
+
 		            	if (B>= C) {
+
 	                System.out.println("\n" + D+ "\n" + A + "\n" + B+"\n" + C);
+
 	            }
+
 	            else
+
 	            {
+
 	                System.out.println("\n" + D + "\n" + A + "\n" + C+ "\n" + B);
+
 	            }
+
 	        }
+
 	    }
 
+
+
 	}
+
+
 
 }
